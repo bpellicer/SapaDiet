2af4c2d2a5fcf07ec527c8679e74824aa98eb903
bernat <b.pellicer@sapalomera.cat>
2021-07-03 21:05:31 +0200

    Problemes amb el responsive
    Mig arreglat, el responsive em porta molt de temps

--- resources/views/components/homePage.blade.php
+++ resources/views/components/homePage.blade.php
@@ -12,44 +12,54 @@
     </div>
 </div>
 
-<div class="contingut mb-10 border-2 border-black" id="infoWeb">
+<div class="container mx-auto mb-10 border-2 border-black" id="infoWeb">
     <div class="w-full text-center font-bold">
         <h1>Coneix l'aplicació!</h1>
     </div>
-    <div class="flex flex-col lg:flex-row sm:mx-10 md:mx-20  border-2 border-black place-content-center">
-        <div class="mx-10 my-5 border-2 border-black shadow-lg h-auto rounded-3xl bg-green1 md:mx-40 lg:mx-10">
-            <h2 class="md:text-2xl text-center my-3 font-bold">Analitza la teva dieta</h2>
-            <p class="px-5 text-justify text-base md:text-xl">Sapa Diet et mostra el teu progrés a través de gràfiques.
-                Monitora les kilocalories, el teu pes corporal i veu el recorregut mensual
-                fins a aconseguir un estil de vida saludable.
-            </p>
-            <div class="flex place-content-center"><img src="/imatges/buscador.png" class="flex carta1"></div>
+    <div class="flex flex-wrap md:mx-2 lg:mx-4">
+        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4">
+            <div class="overflow-hidden rounded-lg shadow-lg bg-green1 border-2 border-black">
+                <h1 class="text-2xl font-bold text-center">Analitza la teva dieta</h1>
+                <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <p class="ml-2 text-sm">
+                            Sapa Diet et mostra el teu progrés a través de gràfiques. Monitora les kilocalories, el teu pes corporal i veu el recorregut mensual fins a aconseguir un estil de vida saludable.
+                        </p>
+                </footer>
+                <img alt="Placeholder" class="block h-auto w-full" src="/imatges/buscador.png">
+            </div>
         </div>
-        <div class="mx-10 my-5 border-2 border-black shadow-lg h-auto rounded-3xl bg-green1 md:mx-40 lg:mx-10">
-            <h2 class="md:text-2xl text-center my-3 font-bold">Escull els aliments que vulguis</h2>
-            <p class="px-5 text-justify text-base md:text-xl">Sapa Diet et mostra el teu progrés a través de gràfiques.
-                Monitora les kilocalories, el teu pes corporal i veu el recorregut mensual
-                fins a aconseguir un estil de vida saludable.
-            </p>
-            <div class="flex place-content-center"><img src="/imatges/pensa.png" class="flex carta2"></div>
+        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4">
+            <div class="overflow-hidden rounded-lg shadow-lg bg-green1 border-2 border-black">
+                <h1 class="text-2xl font-bold text-center">Escull els aliments que</h1>
+                <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <p class="ml-2 text-sm">
+                            Omple la dieta amb els teus aliments preferits i consulta la seva informació nutricional. La dieta la crees tu, així que sigues conscient del que menges!.
+                        </p>
+                </footer>
+                <img alt="Placeholder" class="block h-auto w-full" src="/imatges/pensa.png">
+            </div>
         </div>
-    </div>
-    <div class="flex flex-col lg:flex-row sm:mx-10 md:mx-20  border-2 border-black place-content-center">
-        <div class="mx-10 my-5 border-2 border-black shadow-lg h-auto rounded-3xl bg-green1 md:mx-40 lg:mx-10">
-            <h2 class="md:text-2xl text-center my-3 font-bold">Mira el calendari</h2>
-            <p class="px-5 text-justify text-base md:text-xl">Sapa Diet et mostra el teu progrés a través de gràfiques.
-                Monitora les kilocalories, el teu pes corporal i veu el recorregut mensual
-                fins a aconseguir un estil de vida saludable.
-            </p>
-            <div class="flex place-content-center"><img src="/imatges/calendari.png" class="flex carta3"></div>
+        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4">
+            <div class="overflow-hidden rounded-lg shadow-lg bg-green1 border-2 border-black">
+                <h1 class="text-2xl font-bold text-center">Mira el calendari</h1>
+                <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <p class="ml-2 text-sm">
+                            Observa el calendari mensual i omple cada àpat diari amb els aliments de la teva preferència.
+                        </p>
+                </footer>
+                <img alt="Placeholder" class="block h-auto w-full" src="/imatges/calendari.png">
+            </div>
         </div>
-        <div class="mx-10 my-5 border-2 border-black shadow-lg h-auto rounded-3xl bg-green1 md:mx-40 lg:mx-10">
-            <h2 class="md:text-2xl text-center my-3 font-bold">Crea llistes de la compra</h2>
-            <p class="px-5 text-justify text-base md:text-xl">Sapa Diet et mostra el teu progrés a través de gràfiques.
-                Monitora les kilocalories, el teu pes corporal i veu el recorregut mensual
-                fins a aconseguir un estil de vida saludable.
-            </p>
-            <div class="flex place-content-center"><img src="/imatges/llistacompra.png" class="flex carta3"></div>
+        <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4">
+            <div class="overflow-hidden rounded-lg shadow-lg bg-green1 border-2 border-black">
+                <h1 class="text-2xl font-bold text-center">Crea llistes de la </h1>
+                <footer class="flex items-center justify-between leading-none p-2 md:p-4">
+                        <p class="ml-2 text-sm lg:text-xl">
+                            Podràs gestionar llistes de la compra de forma ràpida i senzilla. Guarda-les i fes-les servir per quan vagis a comprar!.
+                        </p>
+                </footer>
+                <img alt="Placeholder" class="block h-auto w-full" src="/imatges/llistacompra.png">
+            </div>
         </div>
     </div>
 </div>
