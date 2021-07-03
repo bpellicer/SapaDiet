cf728b7bf90ba7ddad01b3abc801fe2d691e886a
bernat <b.pellicer@sapalomera.cat>
2021-07-03 16:03:13 +0200

    Home Page Capçalera finalitzat
    Responsive

--- resources/views/components/homePage.blade.php
+++ resources/views/components/homePage.blade.php
@@ -1,12 +1,12 @@
 <div class="flex flex-wrap mainPage">
     <div class="w-full flex">
         <div class="mx-auto">
-            <img class="float-right hidden lg:flex" src="/imatges/homePageGuy.svg" alt="Un noi menjant una amanida" width="630" height="630">
-            <h1 class=" xl:whitespace-pre-line leading-relaxed sm:leading-relaxed font-bold text-3xl sm:text-5xl px-10 md:px-20 pt-20">Controlar el que
+            <img class="cuate" src="/imatges/homePageGuy.svg" alt="Un noi menjant una amanida">
+            <h1 class="titolHomePage">Controlar el que
                 menges mai havia
                 sigut tan fàcil</h1>
-            <p class="leading-relaxed lg:text-xl px-10 md:px-20">Porta a terme un control de la teva dieta, calcula quantes kilocalories consumeixes al dia, crea llistes de la compra i molt més.</p>
-            <div class="mx-auto px-10 md:px-20 pb-20"><a href="" id = "btnInfo" class="botoEstandar inline-block px-5 py-2 text-center mt-5"><span class="iconify inline-block mr-2" data-inline="false" data-icon="bi:box-arrow-in-right"></span> Llegeix Més !</a></div>
+            <p class="leading-relaxed lg:text-base px-10 md:px-20 lg:pt-5">Porta a terme un control de la teva dieta, calcula quantes kilocalories consumeixes al dia, crea llistes de la compra i molt més.</p>
+            <div class="mx-auto px-10 md:px-20 pb-20 pt-2"><a href="" id = "btnInfo" class="botoEstandar inline-block px-5 py-2 text-center mt-5"><span class="iconify inline-block mr-2" data-inline="false" data-icon="bi:box-arrow-in-right"></span> Llegeix Més !</a></div>
 
         </div>
     </div>
