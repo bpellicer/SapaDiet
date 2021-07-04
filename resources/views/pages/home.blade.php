04842a5071fd070c9945af4f93e89fe14049c96e
bernat <b.pellicer@sapalomera.cat>
2021-07-04 21:06:35 +0200

    Rutes actualitzades
    Hora començament: 16:30
    
    (break a les 18:00 fins les 19:00)
    
    Hora final: 21:05

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,19 +2,19 @@
 <x-nav>
     <x-slot name="linksnav">
         <x-linksnav navclass="navHome" btnclass="sm:hidden">
-            <a href="#" class="linksHome">Inici</a>
-            <a href="#" class="linksHome mt-3">Login</a>
-            <a href="#" class="botoNavHome mt-3">Registra't</a>
+            <a href="/" class="linksHome">Inici</a>
+            <a href="login" class="linksHome mt-3">Login</a>
+            <a href="registre" class="botoNavHome mt-3">Registra't</a>
         </x-linksnav>
     </x-slot>
 </x-nav>
-@include("components.homePage")
+<x-homePage/>
 <x-footer>
     <x-slot name="mapaweb">
         <x-mapaweb>
-            <p class="mt-2"><a href="#" class="linkMapaWeb"> Inici </a></p>
-            <p class="mt-2"><a href="#" class="linkMapaWeb"> Login </a></p>
-            <p class="mt-2"><a href="#" class="linkMapaWeb"> Registra't </a></p>
+            <p class="mt-2"><a href="/" class="linkMapaWeb"> Inici </a></p>
+            <p class="mt-2"><a href="login" class="linkMapaWeb"> Login </a></p>
+            <p class="mt-2"><a href="registre" class="linkMapaWeb"> Registra't </a></p>
         </x-mapaweb>
     </x-slot>
 </x-footer>
