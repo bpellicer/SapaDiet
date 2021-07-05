cfd8971e484257d62539eeec24be8c8840b76e54
bernat <b.pellicer@sapalomera.cat>
2021-07-05 15:05:30 +0200

    Login estàndar

--- resources/views/pages/login.blade.php
+++ resources/views/pages/login.blade.php
@@ -8,7 +8,22 @@
         </x-linksnav>
     </x-slot>
 </x-nav>
-@include("components.homePage")
+<div class="container mx-auto ">
+    <div class="flex justify-center px-10 my-12">
+        <div class="w-full flex sm:justify-center">
+            <div class="w-full bg-green4 lg:w-2/3 p-5 rounded-3xl border-2 border-black">
+                <h3 class="pt-4 text-2xl text-center font-bold">Inicia la sessió</h3>
+                <x-form method="post" class="px-8 pt-6 pb-8">
+                    <x-input tipus="email" nom="email" placeholder="Email"/>
+                    <x-input tipus="password" nom="contrasenya" placeholder="Contrasenya"/>
+                    <div class="text-center">
+                        <x-boto tipus="submit" classe="botoNavHome w-full" text="Entra"/>
+                    </div>
+                </x-form>
+            </div>
+        </div>
+    </div>
+</div>
 <x-footer>
     <x-slot name="mapaweb">
         <x-mapaweb>
