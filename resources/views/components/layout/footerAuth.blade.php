c369be820d10f007b58b18acc29c9ad9f0333766
bernat <b.pellicer@sapalomera.cat>
2021-07-09 20:22:14 +0200

    Update d'última hora footer component

--- resources/views/components/layout/footerAuth.blade.php
+++ resources/views/components/layout/footerAuth.blade.php
@@ -1,18 +1,20 @@
-<x-slot name="mapaweb">
-    <x-mapaweb>
-        <div class="flex flex-wrap">
-            <x-columnaMapaWeb>
-                <x-slot name="titol1">Gestionar Dieta</x-slot>
-                <x-slot name="titol2">Cerca</x-slot>
-            </x-columnaMapaWeb>
-            <x-columnaMapaWeb>
-                <x-slot name="titol1">Llistes Compra</x-slot>
-                <x-slot name="titol2">Planificació</x-slot>
-            </x-columnaMapaWeb>
-            <x-columnaMapaWeb>
-                <x-slot name="titol1">Progrés</x-slot>
-                <x-slot name="titol2">Perfil</x-slot>
-            </x-columnaMapaWeb>
-        </div>
-    </x-mapaweb>
-</x-slot>
+<x-layout.footer>
+    <x-slot name="mapaweb">
+        <x-mapaweb>
+            <div class="flex flex-wrap">
+                <x-columnaMapaWeb>
+                    <x-slot name="titol1">Gestionar Dieta</x-slot>
+                    <x-slot name="titol2">Cerca</x-slot>
+                </x-columnaMapaWeb>
+                <x-columnaMapaWeb>
+                    <x-slot name="titol1">Llistes Compra</x-slot>
+                    <x-slot name="titol2">Planificació</x-slot>
+                </x-columnaMapaWeb>
+                <x-columnaMapaWeb>
+                    <x-slot name="titol1">Progrés</x-slot>
+                    <x-slot name="titol2">Perfil</x-slot>
+                </x-columnaMapaWeb>
+            </div>
+        </x-mapaweb>
+    </x-slot>
+</x-layout.footer>
