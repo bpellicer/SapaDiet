cc207cf8d6ef6e14009eb9b32e7412aa3f36db40
bernat <b.pellicer@sapalomera.cat>
2021-07-13 12:08:58 +0200

    Automatic check radio segons la imatge de l'Usuari

--- resources/views/components/imatges.blade.php
+++ resources/views/components/imatges.blade.php
@@ -1,4 +1,3 @@
-
     <div class="divIntern" id="divIntern" hidden>
         <img src="/imatges/creu.png" class="creu" id="creu">
         <h1 class=" text-2xl">IMATGE DE PERFIL</h1>
@@ -6,7 +5,11 @@
             <div class="gridImatges">
                     @foreach ($imatges as $imatge)
                         <label for="{{$imatge->id}}" class="rounded-full">
-                            <input id="{{$imatge->id}}" type="radio" name="imatge">
+                            <input id="{{$imatge->id}}" type="radio" name="imatge"
+                                @if ($imatge->id === auth()->user()->imatge->id)
+                                    checked
+                                @endif
+                            >
                             <img src ="{{$imatge->url}}" width="150px" class="rounded-full bg-white cursor-pointer imatgePerfil" name="{{$imatge->id}}">
                         </label>
                     @endforeach
@@ -14,9 +17,3 @@
             <x-boto classe="botoEstandar mt-3" tipus="submit" text="Guarda"/>
         </x-form>
     </div>
-    <script>
-        function funcio(id){
-           /*  $("#"+id).attrib("checked"); */
-        }
-
-    </script>
