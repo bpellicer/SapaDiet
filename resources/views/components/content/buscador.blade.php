859d10845f420cca9ec9677e4150ea40b6881036
bernat <b.pellicer@sapalomera.cat>
2021-08-03 19:42:22 +0200

    Petits estils + pujant projecte al drive

--- resources/views/components/content/buscador.blade.php
+++ resources/views/components/content/buscador.blade.php
@@ -1,28 +1,34 @@
-<div class="container bg-green3 border-2 border-black">
-    <table>
-        <thead>
-            <tr>
-                <th>Imatge</th>
-                <th>Categoria</th>
-                <th>Nom</th>
-                <th>Kilocalories</th>
-                <th>Proteines</th>
-                <th>Hidrats</th>
-                <th>Grasses</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($aliments as $aliment)
-                <tr>
-                    <td><img src="{{$aliment->categoria->imatge->url}}" alt=""></td>
-                    <td>{{$aliment->categoria->nom}}</td>
-                    <td>{{$aliment->nom}}</td>
-                    <td>{{$aliment->kilocalories}}</td>
-                    <td>{{$aliment->proteines}}</td>
-                    <td>{{$aliment->hidrats}}</td>
-                    <td>{{$aliment->grasses}}</td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
+<div class="container mx-auto my-auto">
+    <div class="flex justify-center px-10 my-12">
+        <div class="w-full lg:w-9/12 xl:w-10/12 bg-green4 rounded-3xl border-2 border-black p-5 text-center">
+            <div class="flex justify-center">
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Imatge</th>
+                            <th>Categoria</th>
+                            <th>Nom</th>
+                            <th>Kilocalories</th>
+                            <th>Proteines</th>
+                            <th>Hidrats</th>
+                            <th>Grasses</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($aliments as $aliment)
+                            <tr>
+                                <td><img src="{{$aliment->categoria->imatge->url}}" alt=""></td>
+                                <td>{{$aliment->categoria->nom}}</td>
+                                <td>{{$aliment->nom}}</td>
+                                <td>{{$aliment->kilocalories}}</td>
+                                <td>{{$aliment->proteines}}</td>
+                                <td>{{$aliment->hidrats}}</td>
+                                <td>{{$aliment->grasses}}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </div>
 </div>
