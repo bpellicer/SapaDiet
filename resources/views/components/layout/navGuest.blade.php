df9da7f384c13976058b164d787c17df5853e9a3
bernat <b.pellicer@sapalomera.cat>
2021-08-13 20:18:05 +0200

    Canvis NavBar
    DIA 2/20
    
    POMODORO: 8h 20 min

--- resources/views/components/layout/navGuest.blade.php
+++ resources/views/components/layout/navGuest.blade.php
@@ -1,13 +1,13 @@
 <x-layout.nav>
     <x-slot name="linksnav">
         <x-layout.linksnav navclass="navHome" btnclass="sm:hidden">
-            <a href="/" class="linksHome">Inici</a>
+            <div class="flex justify-center"><a href="/" class="linksHome w-10">Inici</a></div>
             @auth
-                <a href="/perfil" class="linksHome mt-3">SapaDiet</a>
+                <div class="flex justify-center"><a href="/perfil" class="linksHome mt-3 w-20">SapaDiet</a></div>
                 <form action="logout" method="post">@csrf<input type="submit" href="logout" class="botoNavHome mt-3" value="Tanca Sessió"></form>
             @endauth
             @guest
-                <a href="/login" class="linksHome mt-3">Login</a>
+                <div class="flex justify-center"><a href="/login" class="linksHome mt-3 w-10">Login</a></div>
                 <a href="/registre" class="botoNavHome mt-3">Registra't</a>
             @endguest
         </x-layout.linksnav>
