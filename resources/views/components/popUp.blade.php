a794671a8ba291de914a75b2eb9dfb9b458c237a
bernat <b.pellicer@sapalomera.cat>
2021-08-27 13:32:40 +0200

    Canvi

--- resources/views/components/popUp.blade.php
+++ resources/views/components/popUp.blade.php
@@ -1,10 +1,9 @@
 <div class="divExtern" id="divExtern2">
-    <div class="divIntern2 w-72 sm:w-96 md:w-100 h-110 sm:h-100" id="divIntern2">
+    <div class="divIntern2 w-64 2xs:w-80 sm:w-96 md:w-100 h-110 sm:h-100" id="divIntern2">
         <img src="/imatges/creu.png" class="creu" id="creu2">
-        <h1 class="text-center font-bold sm:text-xl md:text-3xl text-sm"> Benvingut/da a SAPA DIET! </h1>
-        <h2 class="font-semibold text-xs mt-8 sm:text-xl md:text-2xl">Abans de començar: </h2>
-        <p class="text-justify mt-5 text-sm sm:text-base">Has d'escollir la planificació que més s'ajusti al teu objectiu. Tria els àpats diaris,
-            un aliment de cada categoria com a mínim i el teu objectiu i dóna-li a guardar.
+        <h1 class="text-center font-bold 2xs:text-base sm:text-xl md:text-3xl text-sm"> Benvingut/da a SAPA DIET! </h1>
+        <h2 class="font-semibold text-xs mt-8 2xs:text-base sm:text-xl md:text-2xl">Abans de començar: </h2>
+        <p class="text-justify mt-5 text-sm 2xs:text-base sm:text-base">Escull els àpats diaris, els teus objectius i els aliments preferits que es faran servir per a generar àpats aleatoris i dona-li a guardar.
         </p>
         <p class="text-justify text-xs text-gray-500 mt-4"><span class="text-red-500">*</span> Aquest missatge se't seguirà mostrant fins que canviïs la planificació.
             Si vols mantenir la planificació per defecte, prem el botó de Guardar.
